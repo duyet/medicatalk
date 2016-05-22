4638ba3fcc2cb72a1f74f258861474d0af75c8b7
Van-Duyet Le <lvduit08@gmail.com>
2016-05-22 17:31:17 +0700

    Format api response

--- api/responses/badRequest.js
+++ api/responses/badRequest.js
@@ -1,76 +1,20 @@
 /**
- * 400 (Bad Request) Handler
+ * 400 (Bad Request) Response
  *
- * Usage:
- * return res.badRequest();
- * return res.badRequest(data);
- * return res.badRequest(data, 'some/specific/badRequest/view');
- *
- * e.g.:
- * ```
- * return res.badRequest(
- *   'Please choose a valid `password` (6-12 characters)',
- *   'trial/signup'
- * );
- * ```
+ * The request cannot be fulfilled due to bad syntax.
+ * General error when fulfilling the request would cause an invalid state.
+ * Domain validation errors, missing data, etc.
  */
 
-module.exports = function badRequest(data, options) {
+module.exports = function(data, code, message, root) {
+    var response = _.assign({
+        code: code || 'E_BAD_REQUEST',
+        message: message || 'The request cannot be fulfilled due to bad syntax',
+        data: data || {}
+    }, root);
 
-  // Get access to `req`, `res`, & `sails`
-  var req = this.req;
-  var res = this.res;
-  var sails = req._sails;
+    this.req._sails.log.silly('Sent (400 BAD REQUEST)\n', response);
 
-  // Set status code
-  res.status(400);
-
-  // Log error to console
-  if (data !== undefined) {
-    sails.log.verbose('Sending 400 ("Bad Request") response: \n',data);
-  }
-  else sails.log.verbose('Sending 400 ("Bad Request") response');
-
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
-  if (sails.config.environment === 'production' && sails.config.keepResponseErrors !== true) {
-    data = undefined;
-  }
-
-  // If the user-agent wants JSON, always respond with JSON
-  // If views are disabled, revert to json
-  if (req.wantsJSON || sails.config.hooks.views === false) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // Attempt to prettify data for views, if it's a non-error object
-  var viewData = data;
-  if (!(viewData instanceof Error) && 'object' == typeof viewData) {
-    try {
-      viewData = require('util').inspect(data, {depth: null});
-    }
-    catch(e) {
-      viewData = undefined;
-    }
-  }
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: viewData, title: 'Bad Request' });
-  }
-
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  else return res.guessView({ data: viewData, title: 'Bad Request' }, function couldNotGuessView () {
-    return res.jsonx(data);
-  });
-
+    this.res.status(400);
+    this.res.jsonx(response);
 };
-
